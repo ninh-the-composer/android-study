98fcb83821542b98d65016bce78c99a7493427b7
ninh137 <adzekk01@gmail.com>
2021-05-14 14:23:23 +0700

    Fix bugs Glide does not show image and hero information

--- myAPI/app/src/main/java/fpt/gstpro/myapi/HeroInfomation.java
+++ myAPI/app/src/main/java/fpt/gstpro/myapi/HeroInfomation.java
@@ -3,8 +3,11 @@ package fpt.gstpro.myapi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 
 
 public class HeroInfomation extends AppCompatActivity {
@@ -23,11 +26,28 @@ public class HeroInfomation extends AppCompatActivity {
         String publisher = parent.getStringExtra("publisher") ;
         String imageUrl = parent.getStringExtra("imageUrl") ;
         String bio = parent.getStringExtra("bio");
-        ImageView img = findViewById(R.id.imgAvatar);
-//        Glide.with(this)
-//                .load(imageUrl)
-//                .centerCrop()
-//                .placeholder(R.drawable.ic_launcher_background)
-//                .into(img);
+
+        TextView lbName = findViewById(R.id.lbName);
+        TextView lbRealName = findViewById(R.id.lbRealName);
+        TextView lbTeam = findViewById(R.id.lbTeam);
+        TextView lbCreatedBy = findViewById(R.id.lbCreatedBy);
+        TextView lbFirstAppearance = findViewById(R.id.lbFirstAppearance);
+        TextView lbPublisher = findViewById(R.id.lbPublisher);
+        ImageView img = findViewById(R.id.imageView);
+        TextView lbBio = findViewById(R.id.lbBio);
+
+        lbName.setText(name);
+        lbRealName.setText(realName);
+        lbTeam.setText(team);
+        lbCreatedBy.setText(createdBy);
+        lbFirstAppearance.setText(Integer.toString(firstAppearance));
+        lbPublisher.setText(publisher);
+        lbBio.setText(bio);
+        Glide.with(this)
+                .load(imageUrl)
+                .centerCrop()
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(img);
+
     }
 }
