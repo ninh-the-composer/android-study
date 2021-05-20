d1168ddc136d8bba683bae478f46a55d62861e72
ninh137 <adzekk01@gmail.com>
2021-05-20 15:48:11 +0700

    Refactor code change to use Data Binding instead of findViewById

--- TestCustomDialog/app/src/main/java/fpt/gstpro/testcustomdialog/CustomDialog.java
+++ TestCustomDialog/app/src/main/java/fpt/gstpro/testcustomdialog/CustomDialog.java
@@ -8,22 +8,24 @@ import android.view.Window;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+
+import fpt.gstpro.testcustomdialog.databinding.DialogCustomBinding;
 
 public class CustomDialog extends Dialog {
 
+    DialogCustomBinding binding;
     public CustomDialog(@NonNull Context context) {
         super(context);
-
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.dialog_custom);
-
-        Button btnClose = findViewById(R.id.btnClose);
-        btnClose.setOnClickListener(view -> {
+        binding = DataBindingUtil.inflate(getLayoutInflater(),R.layout.dialog_custom, null, false);
+        setContentView(binding.getRoot());
+        binding.btnClose.setOnClickListener(view -> {
             dismiss();
         });
     }
